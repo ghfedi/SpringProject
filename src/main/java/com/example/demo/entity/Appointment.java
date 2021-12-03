007ebd97939ec3ae995eba31cd98cd2baa052ff5
ghfedi <ghribi.fadi@hotmail.fr>
2021-12-03 10:18:38 +0100

    Update Appointment.java

--- src/main/java/com/example/demo/entity/Appointment.java
+++ src/main/java/com/example/demo/entity/Appointment.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 import java.time.LocalDateTime;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -9,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -23,7 +25,8 @@ public class Appointment {
 	    private long id;
 
 	    private LocalDateTime date;
-
+	    @OneToOne(mappedBy = "appointment",cascade = CascadeType.REMOVE)
+	    private Consultation consultation;
 	    @ManyToOne
 	    @JoinColumn(name = "patient_id")
 	    private Patient patient;
@@ -33,7 +36,7 @@ public class Appointment {
 	    private Doctor doctor;
 
 	    @Enumerated(value = EnumType.STRING)
-	    private AppointmentType appointmentType;
+	    private AppointmentType appointmentType = AppointmentType.ONLINE;
 
 	    private String description;
 	    private Boolean payed;
@@ -41,6 +44,10 @@ public class Appointment {
 	    @Enumerated(value = EnumType.STRING)
 	    private PayementType payementType;
 
+	    public void setPayementType(PayementType type) {
+	    	if(this.appointmentType==AppointmentType.ONLINE)
+	    			type=PayementType.ONLINEPAYEMENT;
+	    }
 
 	    public Appointment() {
 	    }
